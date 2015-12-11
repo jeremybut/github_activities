c7332d84d7646c6bbf9696afcbb1c021ad7c3719
Jeremy Buttice <buttice.j@gmail.com>
2015-12-11 16:38:37 +0100

    revoke credentials | added var env

--- lib/token_authentication.rb
+++ lib/token_authentication.rb
@@ -2,7 +2,7 @@ class TokenAuthentication < Faraday::Middleware
   def call(env)
   	# credentials visible for the test by symbioz
   	# don't forget to replace by var env
-    env[:url].query = env[:url].query.try(:+, '&client_id=74ae307dcfd891286d1e&client_secret=cc3aecf10c129d9cddab4a752b4c5b8be2bedd64&access_token=95a93164cfef00e9b89431681ff6bb5c7bff4f69')
+    # env[:url].query = env[:url].query.try(:+, "&client_id=#{ENV['GITHUB_CLIENT_ID']}&client_secret=#{ENV['GITHUB_CLIENT_SECRET']}&access_token=#{ENV['GITHUB_ACCESS_TOKEN']}")
     @app.call(env)
   end
 end
