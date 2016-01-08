113be6e1a3480cccd62363a572b67c1ec0a24ec2
Buttice <buttice.j@gmail.com>
2016-01-08 09:22:43 +0100

    global bug fix

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'jquery-rails', '~> 4.0', '>= 4.0.5'
 gem 'uglifier', '>= 1.3.0'
 gem 'jbuilder', '~> 2.0'
 gem 'responders', '~> 2.0'
+gem 'githubchart', '~> 1.0', '>= 1.0.1'
 
 gem 'redis-rails', '~> 4.0'
 
--- app/models/repo.rb
+++ app/models/repo.rb
@@ -16,18 +16,32 @@ class Repo
 
   # Get the number of commits per hour in each day
   def commits_per_hour
-    self.class.get("/repos/#{self.owner[:login]}/#{self.name}/stats/punch_card")
+    self.class.fetch_or_get("/repos/#{owner[:login]}/#{name}/stats/punch_card")
     # "25 total commits, during the 2:00pm hour on Tuesdays"    
   end
 
   # Get n commits the week of 'time'
-  def last_year_of_commit(username)
-    arr = self.class.fetch_or_get("/repos/#{username}/#{name}/stats/commit_activity")
-    Hash[arr.map { |key, _value| [key.total, Time.at(key.week)] }] if arr
+  def stats_of_last_year
+    arr = self.class.get("/repos/#{owner[:login]}/#{name}/stats/contributors")
+    if arr.any? && arr
+      weeks = arr.flat_map(&:weeks) 
+      weeks.select {|x| x[:a] + x[:d] + x[:c] > 0 }
+    end
+  end
+
+  def stats_graph_datas
+    arr = self.class.get("/repos/#{owner[:login]}/#{name}/stats/contributors")
+    weeks = arr.flat_map(&:weeks) if !arr.nil?
+    weeks.map { |w| [Time.at(w[:w]).strftime('%Y')+"/"+Time.at(w[:w]).strftime('%m')+"/"+Time.at(w[:w]).strftime('%d'), w[:d] + w[:c] + w[:a] ] }
+  end  
+
+  def init_stats_graph
+    # githubchart -i /path/to/file /path/to/svg
   end
 
   # Position to top n of the user
-  def position(username)
+  def position
+    username = owner[:login]
     contributors = self.class.fetch_or_get("repos/#{username}/#{name}/contributors")
     logins = contributors.map { |x| [x.login] }.flatten
     top_n = logins[0..10]
